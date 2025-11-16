1f6bda52fe656bb0569ddc2a2c2ab326399eecea
amitbishnoi091 <152305040+amitbishnoi091@users.noreply.github.com>
2025-11-16 20:09:31 +0530

    Delete schema directory

--- schema/courier onboarding.sql
+++
@@ -1,8 +0,0 @@
--- Generate synthetic courier onboarding data
-INSERT INTO courier_onboard (activated_at, courier_id, city_id)
-SELECT 
-    DATE_SUB(CURDATE(), INTERVAL FLOOR(RAND()*90) DAY),
-    courier_id,
-    city_id
-FROM couriers
-WHERE status = 'active';
